143d1af9c94235cc62ac226dc419f7cb603150ab
Randall Tom <randalltom@sfo315.ca.revelsystems.com>
2017-11-15 20:29:39 -0800

    1

---
+++ ios-virtual-tourist/Models/Pin+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Pin+CoreDataClass.swift
+//  ios-virtual-tourist
+//
+//  Created by Randall Tom on 11/15/17.
+//  Copyright © 2017 tpb-dev. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Pin)
+public class Pin: NSManagedObject {
+
+}
---
+++ ios-virtual-tourist/Models/Pin+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Pin+CoreDataProperties.swift
+//  ios-virtual-tourist
+//
+//  Created by Randall Tom on 11/15/17.
+//  Copyright © 2017 tpb-dev. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Pin {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Pin> {
+        return NSFetchRequest<Pin>(entityName: "Pin")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+
+}
